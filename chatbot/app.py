8eb4360ce1dde6dd8bf19d9608a277d47fbaa911
Gokuldas CMP <gokuldas127199544@gmail.com>
2025-06-30 22:05:27 +0530

    api created

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,10 +1,13 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
+
+load_dotenv()
 
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 ## Langmith tracking
